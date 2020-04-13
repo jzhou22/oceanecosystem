bb36a33fd792b1817c196bcf16bd846827c0a067
npelletier20 <62970382+npelletier20@users.noreply.github.com>
2020-04-13 14:15:36 -0400

    Update Shark.java

--- Shark.java
+++ Shark.java
@@ -4,11 +4,12 @@ import acm.util.RandomGenerator;
 
 public class Shark extends Fish{
 	
-	public Shark(Location l, World w) {
+	public Shark(Location l, World w, double s) {
 		super(l,w);
 		RandomGenerator rgen = RandomGenerator.getInstance();
 		myLifeSpan = 10;
-		speed=rgen.nextInt(5,10);
+		
+		
 	}
 
 	public void reproduce() {
