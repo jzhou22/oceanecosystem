b7dbb719d82c797b0d4071a0070f2e63ad425655
jzhou22 <62979118+jzhou22@users.noreply.github.com>
2020-04-15 11:16:00 -0700

    Update Clam.java

--- Clam.java
+++ Clam.java
@@ -6,10 +6,18 @@ public class Clam extends LifeForm{
 	
 	public Clam(Location l, World w) {
 		super(l,w);
-		
 		myLifeSpan = 10;
 	}
-
+	
+	//eats kelp in a 5x5 radius
+	public void eat() {
+		for(int x=0; x<5; x++) {
+			for(int y=0; y<5; y++) {
+				
+			}
+		}
+	}
+	
 	public void reproduce() {
 		RandomGenerator rgen = RandomGenerator.getInstance();
 		
